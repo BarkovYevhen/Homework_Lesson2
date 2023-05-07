0a1f932812fe53acf9075fb9c12c7c0b998f6b69
Yevhen Barkov <mr.e.barkov@gmail.com>
2023-05-07 20:53:37 +0300

    Homework 2/1

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-let num1 = prompt("Введіть перше число:");
-let num2 = prompt("Введіть друге число:");
-let num3 = prompt("Введіть третє число:");
-
-num1 = Number(num1);
-num2 = Number(num2);
-num3 = Number(num3);
+let num1 = Number(prompt("Введіть перше число:"));
+let num2 = Number(prompt("Введіть друге число:"));
+let num3 = Number(prompt("Введіть третє число:"));
 
 const average = (num1 + num2 + num3) / 3;
 
